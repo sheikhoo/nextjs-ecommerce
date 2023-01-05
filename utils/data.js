4c0e5cc960ef269105c4e2003e12ccdb170a1f8b
Mohammad Sadegh Sheikh Zahedi <sheikhoo.iran@gmail.com>
2023-01-05 13:01:33 +0330

    first commit

---
+++ utils/data.js
@@ -0,0 +1,42 @@
+const data = {
+  products: [
+    {
+      name: 'تیشرت 1',
+      slug: 't-shirt-1',
+      category: 'Tshirt',
+      image: '/img/tshirt1.jpg',
+      price: 300000,
+      brand: 'Nike',
+      ratting: 4.5,
+      numReviews: 8,
+      countInStock: 10,
+      description: 'تیشرت اصل Nike',
+    },
+    {
+      name: 'تیشرت 1',
+      slug: 't-shirt-1',
+      category: 'Tshirt',
+      image: '/img/tshirt1.jpg',
+      price: 300000,
+      brand: 'Nike',
+      ratting: 4.5,
+      numReviews: 8,
+      countInStock: 10,
+      description: 'تیشرت اصل Nike',
+    },
+    {
+      name: 'تیشرت 1',
+      slug: 't-shirt-1',
+      category: 'Tshirt',
+      image: '/img/tshirt1.jpg',
+      price: 300000,
+      brand: 'Nike',
+      ratting: 4.5,
+      numReviews: 8,
+      countInStock: 10,
+      description: 'تیشرت اصل Nike',
+    },
+  ],
+};
+
+export default data;
